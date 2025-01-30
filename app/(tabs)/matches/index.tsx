880c63aaf4872b1028273696eed81751a83e7fed
Viswanadh <viswanadh@Admins-MacBook-Pro-3.local>
2025-01-30 09:57:47 +0530

    Added MatchCard

--- app/(tabs)/matches/index.tsx
+++ app/(tabs)/matches/index.tsx
@@ -1,9 +1,13 @@
-import { Text } from 'react-native'
+import MatchCard from "@/components/MatchCard";
+import { Text, View } from "react-native";
 
 const live = () => {
   return (
-    <Text>Live</Text>
-  )
-}
+    <View className="p-2">
+      <Text className="text-blue-500 font-bold">Live</Text>
+      <MatchCard />
+    </View>
+  );
+};
 
-export default live
+export default live;
---
+++ components/MatchCard.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const MatchCard = () => {
+  return (
+    <View className="border rounded p-2 shadow-black">
+      <Text className="text-blue-500 font-bold">MatchCard</Text>
+    </View>
+  );
+};
+
+export default MatchCard;
